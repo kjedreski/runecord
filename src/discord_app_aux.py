7c00e06b0fbe23c74ccb3f0bb88bd85c00e4d199
Kevin A Jedreski <jetski07@gmail.com>
2023-08-27 14:28:31 -0600

    added button support, refactored code, removed old commands

---
+++ src/discord_app_aux.py
@@ -0,0 +1,26 @@
+from discord import ButtonStyle, Button
+from discord.ext import commands
+import discord
+from rs_controller import RuneScapeData
+from dkk_controller import DKKData
+import os
+
+
+intents = discord.Intents.default()
+intents.members = True
+client = discord.Client(intents=intents)
+
+
+@client.event
+async def on_ready():
+    print('Bot is ready.')
+
+@client.event
+async def on_member_join(member):
+    channel = discord.utils.get(member.guild.channels, name="general")
+    print(f"hi {member}, channel: {channel}")
+    if channel:
+        await channel.send(f"Welcome to clan DKK, {member.mention}! Any questions? Hit up: Chairboy, tiger or icepick.\n For more info about this server, head over to #clan-dkk-intro.")
+
+
+client.run(os.environ.get('discord_token'))
